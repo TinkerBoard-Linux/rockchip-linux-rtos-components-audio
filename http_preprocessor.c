1c06d15f48c721ff3f1290e89f9d0dc949adf6a2
Jair Wu <jair.wu@rock-chips.com>
2019-09-24 13:01:49 +0000

    enable http preprocessor
    Change-Id: Id0c352ba6073f1049222d67649477e0d164aa67d
    Signed-off-by: Jair Wu <jair.wu@rock-chips.com>

--- http_preprocessor.c
+++ http_preprocessor.c
@@ -5,10 +5,19 @@
  */
 
 #include "AudioConfig.h"
+#include "net/http/http_api.h"
 
-/* int http_socket = -1; */
-/* unsigned char g_is_chunked = 0; */
-/* unsigned long http_totalsize; */
+#define DBG_HTTP_PRE_INFO 0
+
+#if DBG_HTTP_PRE_INFO
+#define DBG_HTTP_PRE(fmt, arg...) rk_printf("[http_player] " fmt "\n", ##arg)
+#else
+#define DBG_HTTP_PRE(fmt, arg...) NULL
+#endif
+
+int http_socket = -1;
+unsigned char g_is_chunked = 0;
+unsigned long http_totalsize;
 
 static int check_http_audio_type(char *target, char **type)
 {
@@ -19,7 +28,7 @@ static int check_http_audio_type(char *target, char **type)
     else if (!strcmp(target, "application/octet-stream"))
     {
         *type = "mp3";  /* consider echocloud platform just provide mp3 */
-        RK_AUDIO_LOG_D("# Content-Type: = %s #\n", target);
+        DBG_HTTP_PRE("# Content-Type: = %s #\n", target);
     }
     else
     {
@@ -31,40 +40,38 @@ static int check_http_audio_type(char *target, char **type)
 int http_preprocessor_init_impl(struct play_preprocessor *self,
                                 play_preprocessor_cfg_t *cfg)
 {
-    return 0;
-#if 0
-    bool bSucc = FALSE;
+    bool bSucc = RK_AUDIO_FAIL;
     int mem_size = HTTP_DOWNLOAD_FILE_NAME_LENGTH;
     char *response = NULL;
 
-    response = (char *) rkos_memory_malloc(mem_size * sizeof(char));
+    response = (char *) audio_malloc(mem_size * sizeof(char));
     http_socket = -1;
     if (http_open(cfg->target, response, &http_socket) != HTTP_STATUS_OK)
     {
         goto END;
     }
 
-    g_is_chunked = FALSE;
+    g_is_chunked = RK_AUDIO_FAIL;
     struct HTTP_RES_HEADER resp = parse_header(response);
-    printf("\n>>>>http header parse success:<<<<\n");
-    printf("\tHTTP reponse: %d\n", resp.status_code);
+    DBG_HTTP_PRE("\n>>>>http header parse success:<<<<\n");
+    DBG_HTTP_PRE("\tHTTP reponse: %d\n", resp.status_code);
     if (resp.status_code != 200 && resp.status_code != 206)
     {
-        printf("file can't be download,status code: %d\n", resp.status_code);
-        bSucc = FALSE;
+        DBG_HTTP_PRE("file can't be download,status code: %d\n", resp.status_code);
+        bSucc = RK_AUDIO_FAIL;
         goto END;
     }
-    printf("\tHTTP file type: %s\n", resp.content_type);
+    DBG_HTTP_PRE("\tHTTP file type: %s\n", resp.content_type);
 
     g_is_chunked = is_chunked(resp);
 
     if (g_is_chunked)
     {
-        printf("\tHTTP transfer-encoding: %s\n", resp.transfer_encoding);
+        DBG_HTTP_PRE("\tHTTP transfer-encoding: %s\n", resp.transfer_encoding);
     }
     else
     {
-        printf("\tHTTP file length: %ld byte\n\n", resp.content_length);
+        DBG_HTTP_PRE("\tHTTP file length: %ld byte\n\n", resp.content_length);
         http_totalsize = resp.content_length;
     }
     cfg->file_size = resp.content_length;
@@ -73,47 +80,44 @@ int http_preprocessor_init_impl(struct play_preprocessor *self,
         int ret = check_http_audio_type(resp.content_type, &cfg->type);
         if (ret != 0)
         {
-            printf("can't find decode type");
-            bSucc = FALSE;
+            DBG_HTTP_PRE("can't find decode type");
+            bSucc = RK_AUDIO_FAIL;
             goto END;
         }
     }
 
     self->userdata = (void *)&http_socket;
     cfg->frame_size = HTTP_PREPROCESSOR_FRAME_SIZE;
-    printf("[%s] open native http ok, http: %s, audio type:%s",
+    DBG_HTTP_PRE("[%s] open native http ok, http: %s, audio type:%s",
            cfg->tag,
            cfg->target,
            cfg->type);
-    bSucc = TRUE;
+    bSucc = RK_AUDIO_TRUE;
 
 END:
     if (response)
-        voice_os_free(response);
+        audio_free(response);
     if (!bSucc)
     {
         if (http_socket != -1)
         {
             close(http_socket);
-            printf("****** close socket. ******\n");
+            DBG_HTTP_PRE("****** close socket. ******\n");
         }
-        return FAIL;
+        return RK_AUDIO_FAILURE;
     }
-    return OK;
-#endif
+    return RK_AUDIO_SUCCESS;
 }
 
 int http_preprocessor_read_impl(struct play_preprocessor *self, char *data,
                                 size_t data_len)
 {
-    return 0;
-#if 0
-    //OS_LOG_D(http_preprocessor,"read data_len:%d",data_len);
-    int rs = FAIL;
+    //DBG_HTTP_PRE("\nread data_len:%d",data_len);
+    int rs = RK_AUDIO_FAIL;
 
     if (!self->userdata)
     {
-        return FAIL;
+        return RK_AUDIO_FAIL;
     }
 
     if (g_is_chunked)
@@ -133,7 +137,7 @@ int http_preprocessor_read_impl(struct play_preprocessor *self, char *data,
         {
             if (rs != data_len)
             {
-                printf("read data underrun,data_len:%d has_read:%d", data_len, rs);
+                DBG_HTTP_PRE("read data underrun,data_len:%d has_read:%d", data_len, rs);
             }
             http_totalsize -= rs;
         }
@@ -141,12 +145,12 @@ int http_preprocessor_read_impl(struct play_preprocessor *self, char *data,
         {
             if (http_totalsize > 0)
             {
-                printf("http read data fail");
+                DBG_HTTP_PRE("http read data fail");
                 return -101; //http not end, return this val for app stop player
             }
             else
             {
-                printf("http read data over");
+                DBG_HTTP_PRE("http read data over");
             }
         }
         else if (rs == -1)
@@ -158,11 +162,9 @@ int http_preprocessor_read_impl(struct play_preprocessor *self, char *data,
     }
 
     return rs;
-#endif
 }
 void http_preprocessor_destroy_impl(struct play_preprocessor *self)
 {
-#if 0
     if (!self)
         return;
     if (!self->userdata)
@@ -172,5 +174,4 @@ void http_preprocessor_destroy_impl(struct play_preprocessor *self)
 
     int client_socket = *(int *)self->userdata;
     http_close(client_socket);
-#endif
 }
