a2f31c43d24b48a2c6048e213c61766cd21dc2de
Sun ChuanHu <aaron.sun@rock-chips.com>
2019-12-10 20:15:33 +0800

    resample: decrease mipis resample in 11_to_44, 16_to_48, filt2000
    Change-Id: Id611845e809d908ce52d3cdf98f26d2633224879
    Signed-off-by: Sun ChuanHu <aaron.sun@rock-chips.com>

--- dsp_include/filt2000.h
+++ dsp_include/filt2000.h
@@ -13,14 +13,14 @@ static  struct
     short sNumPolyPhases;
     short sSampleIncrement;
     short sNumTaps;
-    short sCoefs[2][51];
+    short sCoefs[2][13];
 } SRCFilter_2_0 =
 {
     2,
     1,
-    51,
+    13,
     {
-#if 0
+#if 1
         {   197,  2319,  -307,  -245,  1177, -3655, 25106, 10020, -4650,  2977, -2186,  2124,  1092 },
         {  1092,  2124, -2186,  2977, -4650, 10020, 25106, -3655,  1177,  -245,  -307,  2319,   197 }
 #else
